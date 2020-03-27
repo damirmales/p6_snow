edab1ded2f980bef0f1be6b55e730dd1e5f09bae
damir <55145337+damirmales@users.noreply.github.com>
2020-03-27 23:17:41 +0100

    modify home, figure pages design

--- p6_snow/public/js/load_more.js
+++ p6_snow/public/js/load_more.js
@@ -1,12 +1,18 @@
 function initGridItem(limitSlidePerRow) {
     $("div.grid-item").hide();
     $("div.grid-item").slice(0, limitSlidePerRow).show();
+    
+    $("div.grid-item-photo").hide();
+    $("div.grid-item-photo").slice(0, limitSlidePerRow).show();
+
+    $("div.grid-item-video").hide();
+    $("div.grid-item-video").slice(0, limitSlidePerRow).show();
 }
 
-function loadMore(maxSlice) {
-    console.log('loadmore');
-    $("div.grid-item:hidden").slice(0, maxSlice).slideDown();
+function loadMore(maxSlice, item) {
 
+    $("div." + item + ":hidden").slice(0, maxSlice).slideDown();
+    console.log("div." + item + ":hidden");
     /*  $("#loadMoreFigBtn").on('click', function (e) {
           e.preventDefault();
           $("div.grid-item:hidden").slice(0, 5).slideDown();
