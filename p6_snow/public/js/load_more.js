cb1643c323d892390eb94475a2d540525cf7fe37
damir <55145337+damirmales@users.noreply.github.com>
2020-03-30 12:46:33 +0200

    added bootstrap code to show/hide medias on mobile's screens

--- p6_snow/public/js/load_more.js
+++ p6_snow/public/js/load_more.js
@@ -1,7 +1,7 @@
 function initGridItem(limitSlidePerRow) {
     $("div.grid-item").hide();
     $("div.grid-item").slice(0, limitSlidePerRow).show();
-    
+
     $("div.grid-item-photo").hide();
     $("div.grid-item-photo").slice(0, limitSlidePerRow).show();
 
@@ -13,9 +13,5 @@ function loadMore(maxSlice, item) {
 
     $("div." + item + ":hidden").slice(0, maxSlice).slideDown();
     console.log("div." + item + ":hidden");
-    /*  $("#loadMoreFigBtn").on('click', function (e) {
-          e.preventDefault();
-          $("div.grid-item:hidden").slice(0, 5).slideDown();
 
-      });*/
 }
