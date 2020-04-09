f098e5425cefc73358c020319ec901c6a61430a9
damir <55145337+damirmales@users.noreply.github.com>
2020-04-09 11:17:19 +0200

    Added title in the delete route for photo and video's cards

--- p6_snow/src/Controller/FigureController.php
+++ p6_snow/src/Controller/FigureController.php
@@ -45,7 +45,7 @@ class FigureController extends AbstractController
             //TODO: factorisez l'upload des photos
             if ($imageFile) {
                 $uploadHelper = new ImageUploadHelper();
-                $newImageName = $uploadHelper->imageUploadTest($imageFile, $figure, 'setFeatureImagesta');
+                $newImageName = $uploadHelper->imageUploadTest($imageFile, $figure, 'setFeatureImage');
                 // Move the file to the directory where pictures of figures are stored
                 try {
                     $imageFile->move(
